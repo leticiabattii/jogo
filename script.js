774ff93bf8202669f8130c35d70dd6f63e150eb8
Letícia Abatti <leticiaabattivaz@gmail.com>
2025-03-05 11:01:47 -0300

    criando personagem

--- script.js
+++ script.js
@@ -1,11 +1,54 @@
-//pegar elemento canvas pelo id
+// pegar o elemento canvas pelo id
 const canvas = document.getElementById('jogo2D')
+
 //inicializar o canvas
 const ctx = canvas.getContext('2d')
+const gravidade = 0.5
 
-//criar função loop 
+document.addEventListener('keypress', (e) =>{
+    if (e.code == 'Space' && personagem.pulando == false ){
+        console.log('clicando para pular')
+        personagem.velocidadey = 15
+        personagem.pulando = true 
+    }
+} )
+
+const personagem = { 
+    x: 100,
+    y: canvas.height - 50, 
+    altura: 50, 
+    largura: 50, 
+    velocidadey:0,
+    pulando: false
+}
+function desenharPersonagem(){
+    ctx.fillStyle = 'pink'
+    ctx.fillRect(personagem.x,personagem.y, personagem.altura, personagem.largura)
+}
+function atualizarPersonagem (){
+    //personagem.x +=1
+    if (personagem.pulando == true){
+        personagem.velocidadey -= gravidade
+        personagem.y -= personagem.velocidadey
+        if(personagem.y >= canvas.height -50){
+            personagem.velocidadey = 0
+            personagem.pulando = false
+        }
+
+    }
+    
+}
+//criar a função loop 
 function loop () {
+    //apagar a tela anterior 
+    ctx.clearRect(0,0,canvas.width,canvas.height)
+    //desenhar novamente 
+    desenharPersonagem()
+    //atualizar posições
+    atualizarPersonagem()
+    //chamar o loop denovo 
 
+    requestAnimationFrame(loop)
 }
 
-loop()
+loop ()
