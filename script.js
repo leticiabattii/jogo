3b61935b75f7ecfeb77dd1f233e37b40c4dc252a
Letícia Abatti <leticiaabattivaz@gmail.com>
2025-03-05 09:19:24 -0300

    inicializando js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-a=1
+//pegar elemento canvas pelo id
+const canvas = document.getElementById('jogo2D')
+//inicializar o canvas
+const ctx = canvas.getContext('2d')
+
+//criar função loop 
+function loop () {
+
+}
+
+loop()
