7695f21278c20e8e5f02e0838e7be0ade07c45b3
Letícia Abatti <leticiaabattivaz@gmail.com>
2025-03-12 09:22:14 -0300

    atividade de encapsulamento

--- exercicioClasse.js
+++ exercicioClasse.js
@@ -1,31 +1,89 @@
-class veiculo {
+class Veiculo {
+    #velocidade;
+
     constructor(tipo, marca, cor, velocidade, passageiros) {
         this.tipo = tipo;
         this.marca = marca;
         this.cor = cor;
-        this.velocidade = velocidade;
+        this.#velocidade = velocidade;
         this.passageiros = passageiros;
     }
-    acelerar = function () {
-        this.velocidade += 10
-        console.log(this.velocidade)
+
+    acelerar() {
+        this.#velocidade += 10;
+        console.log(`Velocidade: ${this.#velocidade} km/h`);
     }
-    freiar = function () {
-        if (this.velocidade > 0) {
-            this.velocidade -= 5
-            console.log(this.velocidade)
+
+    freiar() {
+        if (this.#velocidade > 0) {
+            this.#velocidade -= 5;
+            console.log(`Velocidade: ${this.#velocidade} km/h`);
         } else {
-            console.log("O veículo está parado")
+            console.log("O veículo já está parado.");
+        }
+    }
+
+    setVelocidade(valor) {
+        this.#velocidade = valor;
+    }
+
+    getVelocidade() {
+        return this.#velocidade;
+    }
+}
+
+class Aviao extends Veiculo {
+    constructor(tipo, marca, cor, velocidade, passageiros, companhia) {
+        super(tipo, marca, cor, velocidade, passageiros);
+        this.companhia = companhia;
+    }
+}
+
+class Barco extends Veiculo {
+    constructor(tipo, marca, cor, velocidade, passageiros, pier) {
+        super(tipo, marca, cor, velocidade, passageiros);
+        this.pier = pier;
+    }
+
+    getVelocidadeEmNos() {
+        return (this.getVelocidade() / 1.852).toFixed(2); 
+    }
+
+    acelerar() {
+        this.setVelocidade(this.getVelocidade() + 10);
+        console.log(`Velocidade: ${this.getVelocidade()} km/h (${this.getVelocidadeEmNos()} nós)`);
+    }
+
+    freiar() {
+        if (this.getVelocidade() > 0) {
+            this.setVelocidade(this.getVelocidade() - 5);
+            console.log(`Velocidade: ${this.getVelocidade()} km/h (${this.getVelocidadeEmNos()} nós)`);
+        } else {
+            console.log("O barco já está parado.");
         }
     }
 }
 
-const carro = new veiculo('suv','renaut','cinza',0, 0)
-const outro_carro = new veiculo('sedan', 'fiat', 'preto', 0, 0)
-outro_carro.acelerar()
-console.log(carro)
-carro.acelerar()
-carro.acelerar()
-carro.freiar()
-carro.acelerar()
-outro_carro.freiar()
+// Testando as classes
+const carro = new Veiculo('SUV', 'Renault', 'Cinza', 0, 0);
+const outro_carro = new Veiculo('Sedan', 'Fiat', 'Preto', 0, 0);
+outro_carro.acelerar();
+carro.acelerar();
+carro.acelerar();
+carro.freiar();
+carro.freiar();
+
+//parte da atividade é do conteúdo de encapsulamento de objetos (a partir da aula que fala sobre o barco e avião)
+
+const aviao = new Aviao('Comercial', 'Boeing', 'Branco', 0, 0, 'Gol');
+aviao.acelerar();
+aviao.freiar();
+console.log(aviao);
+
+const barco = new Barco('Simples', 'De Pesca', 'Branco', 0, 0, 'Marina');
+barco.acelerar();
+barco.freiar();
+console.log(barco);
+
+aviao.setVelocidade(50);
+console.log(aviao.getVelocidade());
